e168c8f21a0dd0f8e7dd69d01ff69b26f8fdeab8
rattphumi <darkdamonig@gmail.com>
2024-12-06 14:09:19 +0700

    lab7_1_complete

--- lab7_2.cpp
+++ lab7_2.cpp
@@ -16,3 +16,40 @@ Fahsai: Next Monday....that is OK!!! I'm looking forward to watching Star Wars V
 Luke Skywalker: May the Force be with you krub
 Fahsai: 555+ see you Next Monday. Bye Bye \(^ ^)/
 */
+
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main(){
+
+    string name;
+    string movie;
+    string time; //time to with movie
+    string withU; // the fav line in the movie
+    int ID;
+
+    cout << "Fahsai: Sawadee ka...Can you tell me your name?" << endl;
+    cout << "?????: " << name ;    
+    getline(cin,name);
+    cout << "Fahsai: Wow!!! " << name << " is a really cool name." << endl;    
+    cout << "Fahsai: I think you are an Engineering student. What is your student ID?" << endl;    
+    cout << name << ": " ;
+    cin >> ID;    
+    cin.ignore(); // close "\n" overlap
+    cout << "Fahsai: I think you may be GEAR " << (ID/10000000) - 12 << ". I have a free movie ticket for you." << endl;  
+    cout << "Fahsai: Let's go to the cinema together!!!" << endl;    
+    cout << "Fahsai: What movie do you want to watch?" << endl;    
+    cout << name << ": " << movie ;
+    getline(cin,movie);    
+    cout << "Fahsai: So....which day are you free to go with me?" << endl;    
+    cout << name << ": " << time ;
+    getline(cin,time);    
+    cout << "Fahsai: " << time << "....that is OK!!! I'm looking forward to watching " << movie << " with you." << endl;
+    cout << movie << " with you." << endl;    
+    getline(cin,withU);    
+    cout << "Fahsai: 555+ see you " << time << ". Bye Bye \\(^ ^)/" << endl;    
+
+    return 0;
+}
